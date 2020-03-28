6027e4916c106072f0aed152ac441ac7a78b94ba
Suraj Udayashankar <usuraj@opentext.com>
2020-03-28 18:02:34 +0530

    suraj commit

---
+++ precache-manifest.7e80e3cc858cd3ccce1f387b9240d4c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8d8c7af519a6d1fe6b371e3640039e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2edc4f01ad2b4bc55026",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92981ed4d447b4ef908e",
+    "url": "/static/js/2.51981ee5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.51981ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2edc4f01ad2b4bc55026",
+    "url": "/static/js/main.8af06126.chunk.js"
+  },
+  {
+    "revision": "eda9b694fa52801a21a4",
+    "url": "/static/js/runtime-main.7a365d60.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
